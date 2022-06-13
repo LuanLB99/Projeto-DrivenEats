d446a135e2a31625e5120b17e62f8d67798c3d1b
Luan Leal <luanlealboni@gmail.com>
2022-06-13 15:27:26 -0300

    bordas e java script

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function selecionarcomida(elemento) {
    if (comida) {
       comida.classList.remove('borda-verde');
       comida = elemento;
-      comida.classList.add('borda-verde');     
+      comida.classList.add('borda-verde');      
    } else { comida = elemento;
       comida.classList.add('borda-verde');
    }
@@ -46,14 +46,32 @@ function selecionarsobremesa(elemento) {
    liberaPedido();
 }
 
+const precoComida = 0;
+let precoBebida;
+let precoSobremesa;
+let preco = precoComida + precoBebida + precoSobremesa;
+
+let fechar;
+
+
 
 function liberaPedido() {
    if(comida && bebida && sobremesa) {
-      const fechar = document.querySelector('.fundo-pedido');
+      fechar = document.querySelector('.fundo-pedido');
       fechar.classList.add('escondido');
-      const abrir = document.querySelector('.selecionado');
+      abrir = document.querySelector('.selecionado');
       abrir.classList.remove('escondido');
+      let strPedido = `Olá, Gostaria de fazer o pedido: \n
+      - Prato: ${comida.getElementsByTagName('h2')[0].textContent} \n
+      - Bebida: ${bebida.getElementsByTagName('h2')[0].textContent} \n
+      - Sobremesa ${sobremesa.getElementsByTagName('h2')[0].textContent} \n
+      Total: R$ ${preco.toFixed(2)}`;
+      let meuPedido = encodeURIComponent(strPedido);
+      const linkP = document.querySelector('.selecionado');
+      linkP.innerHTML = `<a href="https://wa.me/5512974077007?text=${meuPedido}"><h1>Fechar pedido</h1></a>`;
    }
 }
+
+console.log(comida)
 
 
